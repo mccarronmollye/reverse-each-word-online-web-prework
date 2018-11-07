74fdcc7552b0f2a7428a3ce1c52b15c5db9114b8
Molly McCarron <mccarronmollye@gmail.com>
2018-11-07 12:32:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,5 @@ def reverse_each_word(friendly_reverse_array)
   friendly_reverse_array = "Hello there, and how are you?".split
   friendly_reverse_array.collect do |word|
     word.reverse
-  friendly_reverse_array.join(" ")
 end
 end
